5ac89faf1e48856fee44f8052e04b2ec274fb021
Salvoxea <zcampy@gmail.com>
2026-08-05 19:47:20 +0200

    Remove obsolete Kotlin Android plugin declaration

--- FootballPlayerAndroid/build.gradle.kts
+++ FootballPlayerAndroid/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
     id("com.android.application") version "9.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
 }
