57c05527c67057e484900a02712ee6bba9fc65b7
Salvoxea <zcampy@gmail.com>
2026-08-05 19:47:34 +0200

    Use AGP built-in Kotlin support

--- FootballPlayerAndroid/app/build.gradle.kts
+++ FootballPlayerAndroid/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -24,9 +23,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
     }
 }
 
